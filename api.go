88963be066b312580d7b497596f2d78290318203
Dave Vasilevsky <vasi@stripe.com>
2017-06-24 09:07:21 -0400

    Define an API

---
+++ api.go
@@ -0,0 +1,64 @@
+// Package group_imports allows grouping import statements in Go files.
+//
+// Unlike goimports, it does not maintain existing import statement groups.
+// Instead, it allows defining a canonical order of import statements, and fixes
+// up files to enforce this order.
+//
+// Whatever order of import statements is used, the following rules are
+// enforced:
+//
+// - Import statements within the same group have no empty lines between them.
+// - Between two groups is an empty line.
+package group_imports
+
+import "io"
+
+// A Grouper determines groupings of import statements.
+type Grouper interface {
+	// Group determines the import group that an import statement should be in.
+	//
+	// The input is the package import path, eg: "os" or "github.com/example/repo".
+	//
+	// The output is the group number. If two import statements should be in the same
+	// group, their group number should be identical. If an import statement should
+	// be in a group after another import statement, its group number should be higher.
+	// Otherwise, group numbers are arbitrary. Gaps between group numbers are explicitly
+	// allowed.
+	Group(pkgPath string) (group int)
+}
+
+// Processor processes files according to import grouping rules.
+type Processor struct {
+	grouper Grouper
+}
+
+// NewProcessor creates a new Processor with a given group definition.
+func NewProcessor(grouper Grouper) *Processor {
+	return &Processor{grouper}
+}
+
+// Validate determines whether the existing import grouping of a source file is
+// correct, according to our grouping rules.
+//
+// If the grouping is correct, Validate will return nil, nil.
+// If an unexpected error occurs, Validate returns an error in err.
+// Otherwise, if the grouping is incorrect, Validate returns an error in validError.
+func (p *Processor) Validate(r io.Reader) (validError error, err error) {
+	return p.validate(r)
+}
+
+// Repair repairs the import grouping of a source file.
+//
+// If no repairs are necessary, a nil io.Reader will be returned. If repairs
+// are needed, the new file content will be available in the returned
+// io.Reader.
+func (p *Processor) Repair(r io.Reader) (io.Reader, error) {
+	return p.repair(r)
+}
+
+// Reformat both formats the file with goimports, and repairs any import groupings.
+//
+// The fileName is necessary for determining missing imports.
+func (p *Processor) Reformat(fileName string, r io.Reader) (io.Reader, error) {
+	return p.reformat(fileName, r)
+}
--- group.go
+++ group.go
@@ -1,2 +1,1 @@
 package group_imports
-
